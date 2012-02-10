0497c9e7da6f4877820d82181f9577a55712f36b
Chris Boot <bootc@bootc.net>
2012-02-10 22:43:05 +0000

    firewire-sbp-target: Add sbp_scsi_cmnd.{c,h}
    Miscellaneous functions for dealing with SCSI commands, status, sense
    data and data read/write. This is where the real grunt work of pushing
    data in and out of the FireWire bus happens.
    
    Signed-off-by: Chris Boot <bootc@bootc.net>
    Cc: Andy Grover <agrover@redhat.com>
    Cc: Clemens Ladisch <clemens@ladisch.de>
    Cc: Nicholas A. Bellinger <nab@linux-iscsi.org>
    Cc: Stefan Richter <stefanr@s5r6.in-berlin.de>

---
+++ drivers/target/sbp/sbp_scsi_cmnd.h
@@ -0,0 +1,6 @@
+
+void sbp_handle_command(struct sbp_target_request *req);
+int sbp_rw_data(struct sbp_target_request *req);
+int sbp_send_status(struct sbp_target_request *req);
+int sbp_send_sense(struct sbp_target_request *req);
+void sbp_free_request(struct sbp_target_request *req);
